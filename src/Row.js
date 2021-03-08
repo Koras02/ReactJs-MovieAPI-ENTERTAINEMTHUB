082c23f604b12c1ca97b6cd69be8b10af05d9d1f
Koras02 <wjdgh0727@gmail.com>
2021-03-09 00:52:08 +0900

    Trailer Popup 구현 하기

--- src/Row.js
+++ src/Row.js
@@ -1,47 +1,66 @@
-import React, { useState, useEffect } from 'react'
-import axios from './axios';
-import "./Row.css"
 
-const base_url = "https://image.tmdb.org/t/p/original/"  // 기본 original 주소 가져오기
+import React, { useState, useEffect } from "react";
+import axios from "./axios";
+import "./Row.css";
+import Youtube from "react-youtube";
 
-// 레이아웃 Row 작성
+const baseImgUrl = "https://image.tmdb.org/t/p/original";
+
 function Row({ title, fetchUrl, isLargeRow }) {
     const [movies, setMovies] = useState([]);
+    const [trailerUrl, setTrailerUrl] = useState("");
 
-    //  snippet 사용
+    // Options for react-youtube
+    const opts = {
+        height: "390",
+        width: "100%",
+        playerVars: {
+            autoplay: 1,
+        },
+    };
+
     useEffect(() => {
-        // Tranding Now 부분에 Row 박스 넣기위한 Effect 선언
-        // if [], run once when the row loads, and dont run again 안에 항목이 들어가면 적용 아니면 다시 체크
         async function fetchData() {
-            const request = await axios.get(fetchUrl); // request 로 fetchUrl 
+            const request = await axios.get(fetchUrl);
             setMovies(request.data.results);
             return request;
         }
         fetchData();
     }, [fetchUrl]);
 
-    console.table(movies); // console로 영화 목록 이미지 띄우기
+    const handleClick = async (movie) => {
+        if (trailerUrl) {
+            setTrailerUrl("");
+        } else {
+            let trailerurl = await axios.get(
+                `/movie/${movie.id}/videos?api_key=fb34530271b349314af0de263d16ab5a`
+            );
+            setTrailerUrl(trailerurl.data.results[0]?.key);
+        }
+    };
 
     return (
         <div className="row">
-            {/* Title 부분 */}
             <h2>{title}</h2>
-            {/* Container 부분 */}
-            <div className="row__posters">
-                {/* row 포스터 */}
-                {movies.map((movie) => (
-                    // eslint-disable-next-line no-undef
-                    <img
-                        key={movie.id}
-                        className={`row__poster ${isLargeRow && "row__posterLarge"}`}
-                        src={`${base_url}${isLargeRow ? movie.poster_path : movie.backdrop_path
-                            }`}
-                        alt={movie.name}
-                    />  // 이미지 제목 map함수로 불러온다.
-                ))}
+
+            <div className="row_posters">
+                {movies.map(
+                    (movie) =>
+                        movie.backdrop_path !== null && (
+                            <img
+                                className={`row_poster ${isLargeRow && "row_posterLarge"}`}
+                                src={`${baseImgUrl}${isLargeRow ? movie.poster_path : movie.backdrop_path
+                                    }`}
+                                alt={movie.name}
+                                key={movie.id}
+                                onClick={() => handleClick(movie)}
+                            />
+                        )
+                )}
             </div>
+            {trailerUrl && <Youtube videoId={trailerUrl} opts={opts} />}
         </div>
-    )
+    );
 }
 
-export default Row
+export default Row;
