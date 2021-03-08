5fb5a3b90faa0bdb9ef98be596c770bb4dae77a6
Koras02 <wjdgh0727@gmail.com>
2021-03-08 17:12:41 +0900

    02.api로 영화목록 가져오기 - fetchUrl,base_url 사용

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import './App.css'
+import Row from './Row';
+import requests from './Requests';
+
+// fetchUrl로 requests 에 있는 api 가져오기
 
 function App() {
   return (
     <div className="App">
       <h1>Hey Clever Programmer! Let's build Netflix Clone Front-end today 🚀 </h1>
+      <Row title="NETFLEX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals} />
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
     </div>
   );
 }
+
+
 
 export default App;
